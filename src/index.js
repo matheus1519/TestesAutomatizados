ed8053247952d24b12c5d1046190a60b32628a0a
matheus1519 <1519matheus@gmail.com>
2021-05-27 13:54:24 -0300

    Configuração: eslint e prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (request, response) => {
-  response.json({ message: "App is running!" });
-});
+app.get('/', (request, response) => {
+  response.json({ message: 'App is running!' })
+})
 
-app.listen(3333);
+app.listen(3333)
